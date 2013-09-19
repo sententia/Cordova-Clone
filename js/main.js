2c37fae07afd4d583737ce380a02606fcb34e98d
Rhys Tague <rhys.tague@vcrowd.com>
2013-09-19 17:35:48 +1000

    - Added cordova-clone.js script to the configurator

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 /** This file holds the main function for the settings configurator **/
+
+var cordovaClone = null;
 $(document).ready(function(e) {
 	$('#tabs').tabs();
 	$('input[type=button]').button();
+	cordovaClone = new CordovaClone();
+
+	console.log("Coffee's made.");
 });
+
+/**
+ * Stub for CordovaClone deviceready event
+ * @return {[type]} [description]
+ */
+function main() {}
